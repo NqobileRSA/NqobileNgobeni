4f3d789749c8215fc94d0694c18d1311b33c687e
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-06-10 09:02:21 +0200

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import UImode from "./Mode/UI-mode/UImode";
 import SourceCodeMode from "./Mode/SourceCode-mode/SourceCodeMode";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import AnimatedCursor from "react-animated-cursor";
+// import { emojiCursor } from "cursor-effects";
+// new emojiCursor({ emoji: ["🔥", "🐬", "🦆"] });
 
 AOS.init();
 
@@ -16,6 +19,21 @@ function App() {
 
   return (
     <div className="app">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={55}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "#08fdd8",
+        }}
+        outerStyle={{
+          border: "3px solid #08fdd8",
+        }}
+      />
+
       <button onClick={toggleMode} className="mode-toggler">
         {codeMode ? "UI" : "Source"}
       </button>
